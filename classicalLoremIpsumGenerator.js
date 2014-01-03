afd95c6f3e74bbec63119710121b6a84a033f5bf
Dustin Coates <dustin@dcoates.com>
2014-01-02 22:32:49 -0500

    Fill out classical generator

--- classicalLoremIpsumGenerator.js
+++ classicalLoremIpsumGenerator.js
@@ -2,12 +2,18 @@
 
 function Lorem(text) {
   this.words = [];
+  var defaultPaddingWords = "lorem ipsum dolor sit amet adipisicing elit";
 
   if(this instanceof Lorem){
     if (!text) {
-      text = "lorem ipsum dolor sit amet adipisicing elit";
+      text = defaultPaddingWords;
     }
-    this.words = this.removeDuplicates(text.split(/,\s*|\s/));
+
+    this.words = this.constructor.removeDuplicates(text.split(/,\s*|\s/));
+
+    if(!this.enoughUniqueWords(3)){
+      this.words.concat(defaultPaddingWords.split(" "));
+    }
   } else {
     return new Lorem(text);
   }
@@ -15,10 +21,61 @@ function Lorem(text) {
 }
 
 Lorem.prototype.generate = function(numOfWords) {
+  var wordCounter = 0,
+      output = [],
+      that = this;
 
+  if (isNaN(numOfWords)){
+    numOfWords = parseInt(numOfWords, 10);
+    if (isNaN(numOfWords)){
+      return "You must specify a number.";
+    }
+  }
+
+  function loremCreator(wordCounter, count, periodCounter, wordWithPeriod, sentenceCounter, sentenceWithNewLine){
+    var randomNumber = Math.floor(Math.random() * that.words.length),
+        randomWord = that.words[randomNumber];
+
+    if(wordCounter === count){
+      return that.constructor.sentencize(output);
+    }
+
+    if (!periodCounter || wordWithPeriod < periodCounter){
+      periodCounter = 0;
+      wordWithPeriod = Math.floor(Math.random()*8+5);
+    }
+
+    if (!sentenceCounter || sentenceWithNewLine < sentenceCounter){
+      sentenceCounter = 0;
+      sentenceWithNewLine = Math.floor(Math.random()*4+3);
+    }
+
+    if (randomWord === output[output.length-1] || randomWord === output[output.length-2]){
+      return loremCreator(wordCounter, count, periodCounter,
+        wordWithPeriod, sentenceCounter, sentenceWithNewLine);
+    }
+
+    if (count - wordCounter > 3){
+      if (periodCounter === 0){
+        randomWord = randomWord.charAt(0).toUpperCase() + randomWord.slice(1);
+        if (sentenceCounter === sentenceWithNewLine){
+          randomWord = "\n\n" + randomWord;
+        }
+      }
+      else if (periodCounter === wordWithPeriod ){
+        randomWord = randomWord+".";
+        sentenceCounter++;
+      }
+    }
+
+    output.push(randomWord);
+
+    return loremCreator(wordCounter+1,count,periodCounter+1,wordWithPeriod,sentenceCounter,sentenceWithNewLine);
+  }
+  return loremCreator(wordCounter, numOfWords);
 };
 
-Lorem.prototype.removeDuplicates = function(text) {
+Lorem.removeDuplicates = function(text) {
   var cache = {};
   for (var i=0;i<text.length;i++){
     if (!(text[i] in cache)) {
